88ef470f08db65d3221525e891e99af7d5466a8c
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2020-03-22 18:31:48 +0100

    Factor out replacing x with digit & number extraction

--- missing_digit.go
+++ missing_digit.go
@@ -9,27 +9,31 @@ func MissingDigit(s string) int {
 	tokens := strings.Split(s, " ")
 
 	for i := 0; i < 10; i++ {
-		firstNumAsString := tokens[0]
-		secondNumAsString := tokens[2]
-		thirdNumAsString := tokens[4]
-		if strings.Contains(firstNumAsString, "x") {
-			firstNumAsString = strings.Replace(firstNumAsString, "x", strconv.Itoa(i), 1)
-		}
-		if strings.Contains(secondNumAsString, "x") {
-			secondNumAsString = strings.Replace(secondNumAsString, "x", strconv.Itoa(i), 1)
-		}
-		if strings.Contains(thirdNumAsString, "x") {
-			thirdNumAsString = strings.Replace(thirdNumAsString, "x", strconv.Itoa(i), 1)
-		}
-
-		firstNum, _ := strconv.Atoi(firstNumAsString)
-		secondNum, _ := strconv.Atoi(secondNumAsString)
-		thirdNum, _ := strconv.Atoi(thirdNumAsString)
-
+		firstNum, secondNum, thirdNum := NumbersWithFilledDigit(tokens, i)
 		if firstNum+secondNum == thirdNum {
 			return i
 		}
 	}
 
 	return 0
+}
+
+func NumbersWithFilledDigit(tokens []string, missingDigit int) (int, int, int) {
+	firstNumAsString := tokens[0]
+	secondNumAsString := tokens[2]
+	thirdNumAsString := tokens[4]
+	firstNumAsString = replaceXWithDigit(firstNumAsString, missingDigit)
+	secondNumAsString = replaceXWithDigit(secondNumAsString, missingDigit)
+	thirdNumAsString = replaceXWithDigit(thirdNumAsString, missingDigit)
+	firstNum, _ := strconv.Atoi(firstNumAsString)
+	secondNum, _ := strconv.Atoi(secondNumAsString)
+	thirdNum, _ := strconv.Atoi(thirdNumAsString)
+	return firstNum, secondNum, thirdNum
+}
+
+func replaceXWithDigit(numberAsString string, missingDigit int) string {
+	if strings.Contains(numberAsString, "x") {
+		numberAsString = strings.Replace(numberAsString, "x", strconv.Itoa(missingDigit), 1)
+	}
+	return numberAsString
 }
